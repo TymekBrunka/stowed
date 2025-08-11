2809ced54a9fce3966a97b42de886cd7a127cbf3
TymekBrunka <tymkopolo@wp.pl>
2025-08-11 21:56:47 +0200

    .

---
+++ scripts/scripts/vinegar_studio.sh
@@ -0,0 +1,3 @@
+# cd "$HOME/vinegar/"
+cd "$HOME/scripts/"
+WINEPREFIX="~/.local/share/vinegar/prefixes/studio" GAMEID=0 PROTONPATH=GE-Proton umu-run "$(find ~/.local/share/vinegar/versions/ -name "RobloxStudioBeta.exe")"
